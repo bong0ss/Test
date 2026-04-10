3bedcc495925fbfc4b1d11892330c8030538e1de
Konrad <planetagieror@gmail.com>
2026-04-10 11:33:16 +0200

    Django

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-x = input("Podaj x: ")
-y = input("Podaj y: ")
-print(f"{x} + {y} = " + str(int(x) + int(y)))
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def add():
+    x = float(input("Podaj x: "))
+    y = float(input("Podaj y: "))
+    print(f"{x} + {y} = {x + y}")
+    print(os.getenv("HIDDEN_KEY"))
+
+
+if __name__ == "__main__":
+    add()
