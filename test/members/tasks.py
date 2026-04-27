e3d7631be770d72cf8484080b87719503435c8cd
Konrad <planetagieror@gmail.com>
2026-04-27 09:54:03 +0200

    Task logging + task_id check

--- test/members/tasks.py
+++ test/members/tasks.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import os
 import shutil
 import time
@@ -11,14 +10,14 @@ from celery_progress.backend import ProgressRecorder
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
-from .alarms_TP_to_unified import (
+from .tools.alarms_TP_to_unified import (
     convert_fieldinfo,
     extract_alarm_parameters,
     norm,
     strip_index,
     to_int,
 )
-from .proface_adress_translator import (
+from .tools.proface_adress_translator import (
     change_start_val,
     dbb_to_ls_word,
     dbx_to_ls,
@@ -26,8 +25,6 @@ from .proface_adress_translator import (
     ls_to_dbb_bytes,
 )
 from .utility import custom_data
-
-logger = logging.getLogger(__name__)
 
 
 @shared_task
@@ -60,11 +57,13 @@ def timer(self, time_left, user_id=None):
 
 @shared_task(bind=True)
 def alarms_tp_uni(self, input_xlsx, output_xlsx, input_txt, user_id):
+    custom_data(data=f"Task {self.request.id} started at {time.ctime()}.")
     wb = openpyxl.load_workbook(input_xlsx)
     ws = wb["DiscreteAlarms"]
     og_output_xlsx = output_xlsx
     output_xlsx = str(user_id) + "_" + str(int(time.time())) + "_" + output_xlsx
     # Map headers
+    custom_data(data=f"Task {self.request.id}: Mapping Excel Files.")
     headers = {}
     for c in range(1, ws.max_column + 1):
         v = ws.cell(1, c).value
@@ -75,10 +74,12 @@ def alarms_tp_uni(self, input_xlsx, output_xlsx, input_txt, user_id):
     fieldinfo_col = headers["fieldinfo [alarm text]"]
 
     # ---- Rename headers ----
+    custom_data(data=f"Task {self.request.id}: Renaming headers.")
     ws.cell(1, 3).value = "Alarm text [en-US], Alarm text 1"
     ws.cell(1, 4).value = "FieldInfo [Alarm text 1]"
 
     # ---- Add "Alarm parameter" columns ----
+    custom_data(data=f"Task {self.request.id}: Adding Alarm parameters.")
     ws.insert_cols(ws.max_column + 1)
     param1_col = ws.max_column
     ws.cell(1, param1_col).value = "Alarm parameter 1"
@@ -89,6 +90,7 @@ def alarms_tp_uni(self, input_xlsx, output_xlsx, input_txt, user_id):
     ws.cell(1, param3_col).value = "Alarm parameter 3"
     ws.cell(1, param4_col).value = "Alarm parameter 4"
     # ---- Process rows ----
+    custom_data(data=f"Task {self.request.id}: Processing rows.")
     for r in range(2, ws.max_row + 1):
         # Trigger fix
         tag = ws.cell(r, tag_col).value
@@ -116,11 +118,13 @@ def alarms_tp_uni(self, input_xlsx, output_xlsx, input_txt, user_id):
         ws.cell(r, param2_col).value = params[1] if len(params) >= 2 else "<No value>"
         ws.cell(r, param3_col).value = params[2] if len(params) >= 3 else "<No value>"
         ws.cell(r, param4_col).value = params[3] if len(params) >= 4 else "<No value>"
+    custom_data(data=f"Task {self.request.id}: Processing finished.")
     if os.path.exists(input_xlsx):
         os.remove(input_xlsx)
     wb.save(output_xlsx)
     os.makedirs(f"UserFiles/{str(user_id)}/alarmsTPuni", exist_ok=True)
     shutil.move(output_xlsx, f"UserFiles/{str(user_id)}/alarmsTPuni")
+    custom_data(data=f"Task {self.request.id} finished at {time.ctime()}")
     return mark_safe(
         f'Current task has finished: <a href="{reverse("download", args=[user_id, output_xlsx, og_output_xlsx, "alarmsTPuni"])}">Download</a>'
     )
@@ -128,10 +132,13 @@ def alarms_tp_uni(self, input_xlsx, output_xlsx, input_txt, user_id):
 
 @shared_task(bind=True)
 def proface_adress_translate(self, data, user_id=None):
+    custom_data(data=f"Task {self.request.id} started at {time.ctime()}.")
+    custom_data(data=f"Task {self.request.id}: Changing Starting Values.")
     change_start_val(int(data.get("startdbw")), int(data.get("startls")))
     int_data = data.get("intData", {})
     results = []
     results_json = {}
+    custom_data(data=f"Task {self.request.id}: Reading data for PLC.")
     for i in int_data["plc"]:
         if len(int_data["plc"][i]) > 1:
             results.append(
@@ -142,6 +149,8 @@ def proface_adress_translate(self, data, user_id=None):
             )
         else:
             results.append(dbb_to_ls_word(int(int_data["plc"][i][0].get("word"))))
+    custom_data(data=f"Task {self.request.id}: Adding data for PLC.")
+    custom_data(data=f"Task {self.request.id}: Reading data for Proface.")
     for i in int_data["proface"]:
         if len(int_data["proface"][i]) > 1:
             results.append(
@@ -152,9 +161,11 @@ def proface_adress_translate(self, data, user_id=None):
             )
         else:
             results.append(ls_to_dbb_bytes(int(int_data["proface"][i][0].get("word"))))
-
+    custom_data(data=f"Task {self.request.id}: Adding data for Proface.")
+    custom_data(data=f"Task {self.request.id}: Collecting data.")
     for i, value in enumerate(results):
         results_json[i] = value
+    custom_data(data=f"Task {self.request.id} finished at {time.ctime()}")
     return json.dumps(results_json)
 
 
@@ -170,15 +181,16 @@ def merge_xlsx(
     output_xlsx,
     user_id=None,
 ):
+    custom_data(data=f"Task {self.request.id} started at {time.ctime()}.")
     og_output_xlsx = output_xlsx
     output_xlsx = f"{user_id}_{int(time.time())}_{output_xlsx}"
-
+    custom_data(data=f"Task {self.request.id}: Reading Excel files.")
     df_fix = pandas.read_excel(fix_xlsx)
     df_og = pandas.read_excel(og_xlsx)
 
     og_values = ord(og_values.upper()) - 65
     og_names = ord(og_names.upper()) - 65
-
+    custom_data(data=f"Task {self.request.id}: Checking for missing columns.")
     while len(df_og.columns) <= max(og_names, og_values):
         df_og[f"Column_{df_og.columns[og_names]}"] = None
 
@@ -191,7 +203,9 @@ def merge_xlsx(
 
     first_chunk = True
     chunk_size = 500
-
+    custom_data(
+        data=f"Task {self.request.id}: Starting checking columns in chunks of {chunk_size}'s."
+    )
     for i in range(0, len(df_og), chunk_size):
         df_chunk = df_og.iloc[i : i + chunk_size].copy()
 
@@ -212,13 +226,15 @@ def merge_xlsx(
             )
 
         first_chunk = False
-
+    custom_data(data=f"Task {self.request.id}: Finished checking.")
     os.makedirs(f"UserFiles/{str(user_id)}/FixedXLSX", exist_ok=True)
     shutil.move(output_xlsx, f"UserFiles/{str(user_id)}/FixedXLSX")
+    custom_data(data=f"Task {self.request.id}: Saving file.")
     if os.path.exists(og_xlsx):
         os.remove(og_xlsx)
     if os.path.exists(fix_xlsx):
         os.remove(fix_xlsx)
+    custom_data(data=f"Task {self.request.id} finished at {time.ctime()}")
     return mark_safe(
         f'Current task has finished: <a href="{reverse("download", args=[user_id, output_xlsx, og_output_xlsx, "FixedXLSX"])}">Download</a>'
     )
